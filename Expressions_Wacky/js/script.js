a7a054c7075681c0d0ef33a5c4d7fe44662f8729
Alexander Ruth <aruth09@gmail.com>
2013-07-18 23:08:15 -0400

    Issues with the calculations
    The loan calculations are not working the way I want. Reasearching how
    to fix it.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,14 @@
 
 // Alexander Ruth Wacky Calculator 
 
+var schoolname = prompt("What is the name of your school?");
+var yearsncollege = prompt("How many years spent at your school?");
+var tuition = prompt("How much does" + schoolname +" charge a year?");
+var interestrate = prompt("What is the interest rate on the student loans?");
+
+var monthsincollege = yearsncollege * 12;
+var totaltuition = tuition * yearsncollege;
+var totalintrest = totaltuition * interestrate * monthsincollege;
+var totalcost = totaltuition + totalintrest;
+
+alert("Your cost for tuition at" + schoolname + " after " + yearsncollege + " years is $" + totaltuition + ". The interest alone costs $" + totalintrest + ". The total cost of " + schoolname + " after your loans are paid is $" + totalcost + "."  )
