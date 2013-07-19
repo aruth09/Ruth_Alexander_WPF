a71a6bf9b04c37fc1dd50120f17b072998ec601e
Alexander Ruth <aruth09@gmail.com>
2013-07-18 23:29:22 -0400

    Working Calculator
    The calculator works now. The outputs are what is needed. This code is
    read to be commented.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,12 +4,15 @@
 
 var schoolname = prompt("What is the name of your school?");
 var yearsncollege = prompt("How many years spent at your school?");
-var tuition = prompt("How much does" + schoolname +" charge a year?");
-var interestrate = prompt("What is the interest rate on the student loans?");
+var tuition = prompt("How much does " + schoolname +" charge a year?");
+var interestrate = prompt("What is the interest rate on the student loans in decimals?");
+var yearstopayoff = prompt("How many years is the loan for?");
 
-var monthsincollege = yearsncollege * 12;
+var daystopayoff = yearstopayoff * 365;
 var totaltuition = tuition * yearsncollege;
-var totalintrest = totaltuition * interestrate * monthsincollege;
+var interest = totaltuition * interestrate;
+var dailyintrest = interest / 365;
+var totalintrest = dailyintrest * daystopayoff;
 var totalcost = totaltuition + totalintrest;
 
-alert("Your cost for tuition at" + schoolname + " after " + yearsncollege + " years is $" + totaltuition + ". The interest alone costs $" + totalintrest + ". The total cost of " + schoolname + " after your loans are paid is $" + totalcost + "."  )
+alert("Your cost for tuition at " + schoolname + " after " + yearsncollege + " years is $" + totaltuition + ". The interest alone costs $" + totalintrest + ". The total cost of " + schoolname + " after your loans are paid is $" + totalcost + "."  );
