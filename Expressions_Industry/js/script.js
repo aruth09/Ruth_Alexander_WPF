fea539096321a8bed1985f0c8051b79679736e85
Alexander Ruth <aruth09@gmail.com>
2013-07-18 23:46:56 -0400

    Finished product
    Comments have been added inline to explain code. The calculators work
    and are ready to be graded.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,9 +1,9 @@
 // JavaScript
 // Alexander Ruth Industry Calculator
 
-var sitewidth = prompt("In pixels how wide is your site?");
-var sitecolumns = prompt("How many columns will your site have?");
-var sitemargins = prompt("In pixels what magin spaceing do you want?");
-var columnwidth = (sitewidth / sitecolumns) - sitemargins;
+var sitewidth = prompt("In pixels how wide is your site?"); // gathering how many pixels the site is wide storing in variable
+var sitecolumns = prompt("How many columns will your site have?"); // gathereing how many columns the site will have storing in variable
+var sitemargins = prompt("In pixels what magin spaceing do you want?"); // gathereing the margins in pixels and storing in variables
+var columnwidth = (sitewidth / sitecolumns) - sitemargins; // calculating the column widths and subtracting the site margins from that, storing in variables
 
-alert("Your site has " + sitecolumns + " columns. Each column is " + columnwidth + "px with margins of " + sitemargins + "px.");
+alert("Your site has " + sitecolumns + " columns. Each column is " + columnwidth + "px with margins of " + sitemargins + "px."); // printing the out put of our calulations in an alert popup 
--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,17 +2,17 @@
 
 // Alexander Ruth Wacky Calculator 
 
-var schoolname = prompt("What is the name of your school?");
-var yearsncollege = prompt("How many years spent at your school?");
-var tuition = prompt("How much does " + schoolname +" charge a year?");
-var interestrate = prompt("What is the interest rate on the student loans in decimals?");
-var yearstopayoff = prompt("How many years is the loan for?");
+var schoolname = prompt("What is the name of your school?"); // gathering the users school name storing in variable
+var yearsncollege = prompt("How many years spent at your school?"); // gatering the time spent in school storing in variable
+var tuition = prompt("How much does " + schoolname +" charge a year?"); // gathering the students tuition cost, showing school name to make personable, storing in variable
+var interestrate = prompt("What is the interest rate on the student loans in decimals?"); // gathering intrest rate on loans, storing in variable
+var yearstopayoff = prompt("How many years is the loan for?"); // gathering the years to pay debt off storing in variable
 
-var daystopayoff = yearstopayoff * 365;
-var totaltuition = tuition * yearsncollege;
-var interest = totaltuition * interestrate;
-var dailyintrest = interest / 365;
-var totalintrest = dailyintrest * daystopayoff;
-var totalcost = totaltuition + totalintrest;
+var daystopayoff = yearstopayoff * 365; // converting years to days soring in variable
+var totaltuition = tuition * yearsncollege; // calculating the total tuition, storing in variable
+var interest = totaltuition * interestrate; // calculating the intrest before devideing by days storing in variable
+var dailyintrest = interest / 365; // calculating the intrest per day stroring in variable
+var totalintrest = dailyintrest * daystopayoff; // calulating the total intrest cost storing in variable
+var totalcost = totaltuition + totalintrest; // calulating the toatal cost of the school tuition plus intrest storing in variable
 
-alert("Your cost for tuition at " + schoolname + " after " + yearsncollege + " years is $" + totaltuition + ". The interest alone costs $" + totalintrest + ". The total cost of " + schoolname + " after your loans are paid is $" + totalcost + "."  );
+alert("Your cost for tuition at " + schoolname + " after " + yearsncollege + " years is $" + totaltuition + ". The interest alone costs $" + totalintrest + ". The total cost of " + schoolname + " after your loans are paid is $" + totalcost + "."  ); // printing the calculations of total tuition, total intrest, and total cost of the school in a pop up alert.
