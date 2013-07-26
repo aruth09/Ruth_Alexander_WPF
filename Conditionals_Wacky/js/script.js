a2f02931783215713199b53e7e72e05500000603
Alexander Ruth <aruth09@gmail.com>
2013-07-25 22:40:44 -0400

    Code was broke
    Changed the format of one of the if statements. Testing again.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,10 +7,11 @@ var homework = prompt("Do you have homework?(true/false)");
 var sleepneeded = 6;
 var getreadytime = 2;
 var sleep = worktime - sleepneeded - getreadytime;
-(worktime < 6) ?  alert("Stay in and get some sleep.");
-	else if (moneynacount < 200) 
-		{alert("You have no money, stay home.");}
-			else if (homework) 
-				{alert("You need to stay home and work on homework.");}
-				else 
-					{alert("Go to the bar and have some fun. Be in bed by " + sleep + "am to get some sleep to be functional at work.");}
+if (worktime < 6)  
+	{alert("Stay in and get some sleep.");}
+		else if (moneynacount < 200) 
+			{alert("You have no money, stay home.");}
+				else if (homework) 
+					{alert("You need to stay home and work on homework.");}
+					else 
+						{alert("Go to the bar and have some fun. Be in bed by " + sleep + "am to get some sleep to be functional at work.");}
