5c28456eda1ad32924eb3b22d0d87f578c1f7f0e
Alexander Ruth <aruth09@gmail.com>
2013-07-25 22:36:21 -0400

    Completely reinvented wacky calulator

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,13 +1,16 @@
 // JavaScript 
 // Alexander Ruth Conditional Wacky
 
-var coffeeamount = prompt("How many cups of coffee a week do you buy?");
-var coffeeprice = prompt("How much in dollars is your average cup of coffee cost?");
-var cigarettesamount = prompt("How many packs of cigarettes do you buy?(if any)");
-var cigarettesprice = prompt("How much is your brand of cigarettes per pack?");
-var beercost = prompt("How much do you spend on beer a week?(guess)");
-var popamount = prompt("How many pops a week do you buy");
-var popprice = prompt("How are you paying a pop?");
-var gumamount = prompt("How many packs of gum are you buying?");
-var gumprice = prompt("How much is a pack of gum?");
-var snacksandcandyprice = prompt("How much are you spending on candy a week?(another guess)");
+var worktime = prompt("What time do you work at?");
+var moneynacount = prompt("How much money do you have?");
+var homework = prompt("Do you have homework?(true/false)");
+var sleepneeded = 6;
+var getreadytime = 2;
+var sleep = worktime - sleepneeded - getreadytime;
+(worktime < 6) ?  alert("Stay in and get some sleep.");
+	else if (moneynacount < 200) 
+		{alert("You have no money, stay home.")};
+			else if (homework) 
+				{alert("You need to stay home and work on homework.")};
+				else 
+					{alert("Go to the bar and have some fun. Be in bed by " + sleep + "am to get some sleep to be functional at work.")};
