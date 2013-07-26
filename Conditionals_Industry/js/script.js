1bbf7225fa1cef587beaec210cc84b9ceeaf96bf
Alexander Ruth <aruth09@gmail.com>
2013-07-25 21:35:05 -0400

    Code written ready to test
    coments have not been made still need to test code as well.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,4 +1,16 @@
 // JavaScript 
 // Alexander Ruth Conditional Industry
 
-
+var currentjobhours = prompt("How many hours are committed to current projects?");
+var proposedjobhours = prompt("How many hours will the Job take?");
+var proposedjobhourlypay = prompt("How much will the Job pay per hour?");
+var desiredhourlypay = 20;
+var maxhourscommitted = 100;
+var takejob = false;
+var proposedhourscommitted = currentjobhours + proposedjobhours;
+if (proposedhourscommitted <= maxhourscommitted && desiredhourlypay <= proposedjobhourlypay) 
+	{takejob = true;}
+	else{takejob=false;}
+if (takejob) 
+	{alert("Take the job! The money is good and you currently have time.")};
+	else{alert("Do not take the job, you are booked and can not work for free.")}
