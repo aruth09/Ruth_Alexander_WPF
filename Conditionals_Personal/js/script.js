4ea9fc192ec0a71b82e26974cf44aee5598f5a4c
Alexander Ruth <aruth09@gmail.com>
2013-07-25 20:28:28 -0400

    Variables created
    Created the variables for the Should you buy the car calculator.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,10 @@
 // JavaScript 
 // Alexander Ruth Conditional Personal
 
+var carmileage = prompt("What is the cars mileage?");
+var caryear = prompt("What year is the car?");
+var conditionalmileage = 1000000;
+var conditionalyear = 2004;
+var bonusmileage = 30000;
+var bonusyear = 2010;
+
