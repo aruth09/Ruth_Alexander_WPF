2779e5084caa87c7b6d496de9d47b7c9915b0141
Alexander Ruth <aruth09@gmail.com>
2013-07-25 22:14:35 -0400

    Project started
    Variables have been decleared ready to insert the calculations and if
    statements.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,13 @@
 // JavaScript 
 // Alexander Ruth Conditional Wacky
 
+var coffeeamount = prompt("How many cups of coffee a week do you buy?");
+var coffeeprice = prompt("How much in dollars is your average cup of coffee cost?");
+var cigarettesamount = prompt("How many packs of cigarettes do you buy?(if any)");
+var cigarettesprice = prompt("How much is your brand of cigarettes per pack?");
+var beercost = prompt("How much do you spend on beer a week?(guess)");
+var popamount = prompt("How many pops a week do you buy");
+var popprice = prompt("How are you paying a pop?");
+var gumamount = prompt("How many packs of gum are you buying?");
+var gumprice = prompt("How much is a pack of gum?");
+var snacksandcandyprice = prompt("How much are you spending on candy a week?(another guess)");
