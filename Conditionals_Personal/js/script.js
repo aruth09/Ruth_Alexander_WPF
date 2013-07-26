f21e75be4cd5d6ae30ff740bef0ae5a2aef80cf3
Alexander Ruth <aruth09@gmail.com>
2013-07-25 21:02:52 -0400

    testing
    having problems with the else function

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,4 +7,14 @@ var conditionalmileage = 1000000;
 var conditionalyear = 2004;
 var bonusmileage = 30000;
 var bonusyear = 2010;
-
+var buycar = false;
+if(carmileage <= conditionalmileage)
+	{buycar = true;}
+		else{buycar = false;}
+if(caryear => conditionalyear)
+	{buycar = true;}
+		else{buycar = false;}
+if(buycar)
+	{consel.log ("Go ahead and buy the car. Its gonna be a great car.");}
+		else
+			{consel.log ("Don't buys the car, its either old or had to many miles. Keep looking.");}
