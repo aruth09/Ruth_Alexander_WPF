f4f18101123f0d19e52c2c4c55228d0fe703fab3
Alexander Ruth <aruth09@gmail.com>
2013-08-01 21:31:59 -0400

    Work has begun
    Variables have been created as well as two functions.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,16 @@
 // JavaScript
 // Alexander Ruth Personal Calculator
 
+var usergallons = prompt("How many gallons of gas can your car's tank hold?");
+var usermiles = prompt("How many miles can you drive on a tank of gas?(an average works)");
+var workdistancemiles = prompt("How far is work from your house?");
+var additionalmiles = prompt("Are you driving any extra miles in the next few days?");
+var currenttankmiles = prompt("How many miles are on your current tank?(How far have you drivin?)");
+
+function convertMPGs(usergallons, usermiles){
+	var mpgs = usermiles / usergallons;
+}
+
+function daytowork(workdistancemiles){
+	var workday = workdistancemiles * 2;
+}
