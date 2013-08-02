046af8f58cba2fc9aa7fa2ae145a12def746ab4a
Alexander Ruth <aruth09@gmail.com>
2013-08-01 22:23:43 -0400

    The testing saga continues

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -9,6 +9,7 @@ var currenttankmiles = prompt("How many miles are on your current tank?(How far 
 var mpgs = 0;
 var workday = 0;
 var dayslefttodrive = 0;
+var milesleftintank = 0;
 
 function convertMPGs(usergallons, usermiles){
 	mpgs = usermiles / usergallons;
@@ -19,16 +20,18 @@ function daytowork(workdistancemiles, workday){
 	workday = workdistancemiles * 2;
 }
 
-function daysleft(usermiles, currenttankmiles, workday){
+function daysleft(usermiles, currenttankmiles, workday, milesleftintank, additionalmiles){
 	additionalmiles = prompt("Are you driving any extra miles in the next few days?");
-	dayslefttodrive = (usermiles - currenttankmiles - additionalmiles)/workday;
+	milesleftintank = usermiles - currenttankmiles - additionalmiles;
+	dayslefttodrive = milesleftintank/workday;
+	return dayslefttodrive;
 }
 
-function resultsprint(currenttankmiles, dayslefttodrive, mpgs){
+function resultsprint(dayslefttodrive, mpgs){
 	console.log("You have " + dayslefttodrive + " days left you can drive to work. " + "Also your car gets " + mpgs + " miles to the gallon.");
 }
 
 convertMPGs(usergallons, usermiles);
 daytowork(workdistancemiles, workday);
-daysleft(usermiles, currenttankmiles, workday);
-resultsprint(currenttankmiles, dayslefttodrive, mpgs);
+daysleft(usermiles, currenttankmiles, workday, milesleftintank, additionalmiles);
+resultsprint(dayslefttodrive, mpgs);
