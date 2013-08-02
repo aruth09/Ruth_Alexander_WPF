dc8d7db8c777be6eb63df00c0cfbb5bfd11c8861
Alexander Ruth <aruth09@gmail.com>
2013-08-01 21:55:31 -0400

    Functions added[testing]
    Functions have been added. Testing now.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,8 +4,10 @@
 var usergallons = prompt("How many gallons of gas can your car's tank hold?");
 var usermiles = prompt("How many miles can you drive on a tank of gas?(an average works)");
 var workdistancemiles = prompt("How far is work from your house?");
-var additionalmiles = prompt("Are you driving any extra miles in the next few days?");
+var additionalmiles = 0;
 var currenttankmiles = prompt("How many miles are on your current tank?(How far have you drivin?)");
+var mpgs
+var workday
 
 function convertMPGs(usergallons, usermiles){
 	var mpgs = usermiles / usergallons;
@@ -14,3 +16,17 @@ function convertMPGs(usergallons, usermiles){
 function daytowork(workdistancemiles){
 	var workday = workdistancemiles * 2;
 }
+
+function daysleft(usermiles, currenttankmiles, workday){
+	var additionalmiles = prompt("Are you driving any extra miles in the next few days?");
+	var dayslefttodrive = (usermiles - currenttankmiles - additionalmiles)/workday;
+}
+
+function resultsprint(currenttankmiles, dayslefttodrive, mpgs){
+	console.log("You have " + dayslefttodrive + " days left you can drive to work. " + "Also your car gets " + mpgs + " miles to the gallon.");
+}
+
+convertMPGs();
+daytowork();
+daysleft();
+
